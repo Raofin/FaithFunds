ed888ea48ccf24106dab48a738639dbee131b39b
Zaid Amin <zaidaminraofin@gmail.com>
2023-08-19 06:42:26 +0600

    Set look n feel to Windows

--- src/main/java/com/MosjidDonation/MosjidDonation.java
+++ src/main/java/com/MosjidDonation/MosjidDonation.java
@@ -4,6 +4,8 @@
 
 package com.MosjidDonation;
 
+import com.MosjidDonation.ui.WelcomeJFrame;
+
 /**
  *
  * @author Raofin
@@ -11,6 +13,27 @@ package com.MosjidDonation;
 public class MosjidDonation {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Set the look and feel of the GUI to match the Windows style
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            // If there's an error in setting the look and feel, log the error
+            java.util.logging.Logger.getLogger(WelcomeJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        // Start the GUI in the Event Dispatch Thread
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                // Create an instance of WelcomeJFrame and set its properties
+                WelcomeJFrame homeFrame = new WelcomeJFrame();
+                homeFrame.setLocationRelativeTo(null); // Center the frame on the screen
+                homeFrame.setVisible(true); // Make the frame visible
+            }
+        });
     }
 }
